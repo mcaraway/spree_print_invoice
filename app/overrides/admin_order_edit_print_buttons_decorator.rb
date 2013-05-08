806920d5ebc4c3563cf3916403a2ef2f8276cec3
unknown <Michael@.(none)>
2013-05-08 07:26:50 -0400

    changed to show button on the new page_actions header

--- app/overrides/admin_order_edit_print_buttons_decorator.rb
+++ app/overrides/admin_order_edit_print_buttons_decorator.rb
@@ -1,5 +1,6 @@
-Deface::Override.new(:virtual_path => "spree/admin/orders/edit",
-                     :name => "print_buttons",
-                     :insert_bottom => "[data-hook='admin_order_edit_buttons'], #admin_order_edit_buttons[data-hook]",
+Deface::Override.new(:virtual_path => "spree/admin/orders/show",
+                     :name => "print_buttons_2",
+                     :insert_before => "[data-hook='admin_order_edit_header'], #admin_order_edit_header[data-hook]",
                      :partial => "spree/admin/orders/print_buttons",
-                     :disabled => false)
+                     :disabled => false,
+                     :original => '3e847740dc3e7fasda1ccb56645466676j734hskk')
--- app/overrides/admin_order_show_print_buttons_decorator.rb
+++ app/overrides/admin_order_show_print_buttons_decorator.rb
@@ -1,5 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/show",
                      :name => "print_buttons",
-                     :insert_bottom => "[data-hook='admin_order_show_buttons'], #admin_order_show_buttons[data-hook]",
+                     :insert_before => "[data-hook='admin_order_show_addresses'], #admin_order_show_addresses[data-hook]",
                      :partial => "spree/admin/orders/print_buttons",
-                     :disabled => false)
+                     :disabled => false,
+                     :original => '3e847740dc3e7fasda1ccb56645466676j734hskk')                     
